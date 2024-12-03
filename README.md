2b1934dddf70fc180e8f42492c0b14fba95ba561
Siam Hasan <hasan.2002.siam@gmail.com>
2024-12-03 02:29:09 -0800

    initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Welcome to Sili
+A basic CLI app through which you can get lot of stuff done from port scanning to many more
---
+++ bin/sili.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "HOST: $1"
+echo "PORT: $2"
+mkdir dumb
+touch dumb/dumb.txt
+echo "You are a dumb" > dumb/dumb.txt
